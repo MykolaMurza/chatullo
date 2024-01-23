172314eb08788834017bc8239d1fc9488b7fb8be
justADeni <deninikdo@gmail.com>
2024-01-23 16:35:35 +0100

    re-added payment with items for global chat

--- src/main/java/ua/mykolamurza/chatullo/listeners/PlayerChat.java
+++ src/main/java/ua/mykolamurza/chatullo/listeners/PlayerChat.java
@@ -3,9 +3,11 @@ package ua.mykolamurza.chatullo.listeners;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 import ua.mykolamurza.chatullo.configuration.Config;
 import ua.mykolamurza.chatullo.handler.ChatHandler;
 import ua.mykolamurza.chatullo.handler.MessageType;
@@ -27,6 +29,32 @@ public class PlayerChat implements Listener {
             if (!player.hasPermission("chatullo.global")){
                 player.sendMessage(chatHandler.formatMessage(MessageType.OTHER, player, Config.messages.getString("error.permission")));
                 return;
+            }
+
+            if (Config.settings.getBoolean("global-pay.item.enabled")) {
+                ItemStack item = null;
+                if (player.getInventory().getItemInMainHand().getType() == Material.valueOf(Config.settings.getString("global-pay.item.material")))
+                    item = player.getInventory().getItem(player.getInventory().getHeldItemSlot());
+                else if (player.getInventory().getItemInOffHand().getType() == Material.valueOf(Config.settings.getString("global-pay.item.material")))
+                    item = player.getInventory().getItem(40);
+
+                if (item == null) {
+                    player.sendMessage(chatHandler.formatMessage(MessageType.OTHER, player,
+                            Config.messages.getString("error.item").replace("%amount%", String.valueOf(Config.settings.getInt("global-pay.item.amount")))));
+                    return;
+                }
+
+                if (item.getAmount() < Config.settings.getInt("global-pay.item.amount")) {
+                    player.sendMessage(chatHandler.formatMessage(MessageType.OTHER, player,
+                            Config.messages.getString("error.item").replace("%amount%", String.valueOf(Config.settings.getInt("global-pay.item.amount")))));
+                    return;
+                }
+
+                item.setAmount(item.getAmount() - Config.settings.getInt("global-pay.item.amount"));
+            }
+
+            if (Config.settings.getBoolean("global-pay.vault.enabled")) {
+                // TODO: Vault integration
             }
 
             chatHandler.writeToGlobalChat(event, player, message.content().substring(1));
